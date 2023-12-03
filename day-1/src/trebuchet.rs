866c0fe8d4ca9c804a05a378fa7aecf606416c6a
Sviatoslav Shkut <sgshkut@gmail.com>
2023-12-03 18:57:07 +0100

    Day 1, part 2

--- day-1/src/trebuchet.rs
+++ day-1/src/trebuchet.rs
@@ -1,12 +1,42 @@
 pub fn process(data: &str) -> u32 {
-    data.lines().map(extract_calibration_values).sum()
+    data.lines()
+        .map(extract_calibration_values)
+        .sum()
 }
 
 fn extract_calibration_values(line: &str) -> u32 {
-    let numbers: String = line.chars().filter(|c| c.is_numeric()).collect();
-    let first = numbers.chars().next().unwrap_or('0');
-    let last = numbers.chars().last().unwrap_or('0');
-    format!("{}{}", first, last).parse::<u32>().unwrap()
+    let mut numbers = (0..line.len()).filter_map(|index| {
+        let reduced_line = &line[index..];
+        let result = if reduced_line.starts_with("one") {
+            Some(1)
+        } else if reduced_line.starts_with("two") {
+            Some(2)
+        } else if reduced_line.starts_with("three") {
+            Some(3)
+        } else if reduced_line.starts_with("four") {
+            Some(4)
+        } else if reduced_line.starts_with("five") {
+            Some(5)
+        } else if reduced_line.starts_with("six") {
+            Some(6)
+        } else if reduced_line.starts_with("seven") {
+            Some(7)
+        } else if reduced_line.starts_with("eight") {
+            Some(8)
+        } else if reduced_line.starts_with("nine") {
+            Some(9)
+        } else {
+            reduced_line.chars().next().unwrap().to_digit(10)
+        };
+
+        result
+    });
+    let first = numbers.next().unwrap_or(0); 
+
+    match numbers.last() {
+        Some(number) => first * 10 + number,
+        None => first * 10 + first,
+    }
 }
 
 #[cfg(test)]
@@ -14,16 +44,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_process() {
-        let data = "3two9eight7dl5\n\
-            6five2thone65\n\
-            fsevensix916\n\
-            nejzbpzone7\n";
-        let expected = 35 + 65 + 96 + 77;
+    #[ignore]
+    fn test_process_part1() {
+        let data = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        let expected = 12 + 38 + 15 + 77;
         let result = process(&data);
         assert_eq!(expected, result);
     }
 
+    #[test]
+    fn test_process_part2() {
+        let data = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+onethreenfkgrvsevenkczctlgkt7
+";
+        let expected = 29 + 83 + 13 + 24 + 42 + 14 + 76 + 17;
+        let result = process(&data);
+        assert_eq!(expected, result);
+    }
     #[test]
     fn test_process_empty() {
         let data = "";
